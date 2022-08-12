b0e09c5ea727456a7d709b9480924a8d348abcfc
Raphael Carvalho <raphilskerj@gmail.com>
2022-08-12 19:38:12 -0300

    Solução primeiro desafio do Bootcamp Cypress Advanced

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,29 +1,81 @@
-describe('Login', () => {
-  it('deve logar com sucesso', () => {
-    const user = {
-      name: 'Raphael',
-      instagram: '@raphilskerj',
-      password: 'pwd123'
-    }
-    cy.login(user)
-    cy.loggedUser('Raphael')
+describe('login', () => {
+
+  context('com credenciais válidas', () => {
+    it('deve logar com sucesso', () => {
+      const user = {
+        name: 'Raphael',
+        instagram: '@raphilskerj',
+        password: 'pwd123'
+      }
+      cy.login(user)
+      cy.loggedUser('Raphael')
+    })
   })
 
-  it('não deve logar com senha incorreta', () => {
-    const user = {
-      instagram: '@raphilskerj',
-      password: '123456'
-    }
-    cy.login(user)
-    cy.modalHaveText('Credenciais inválidas, tente novamente!')
+  context('com senha incorreta', () => {
+    it('deve notificar falha de credenciais', () => {
+      const user = {
+        instagram: '@raphilskerj',
+        password: '123456'
+      }
+      cy.login(user)
+      cy.modalHaveText('Credenciais inválidas, tente novamente!')
+    })
   })
 
-  it('não deve logar com instagram inexistente', () => {
-    const user = {
-      instagram: '@raphilskerj',
-      password: '123456'
-    }
-    cy.login(user)
-    cy.modalHaveText('Credenciais inválidas, tente novamente!')
+  context('com instagram inexistente', () => {
+    it('não deve logar com instagram inexistente', () => {
+      const user = {
+        instagram: '@raphilskerj',
+        password: '123456'
+      }
+      cy.login(user)
+      cy.modalHaveText('Credenciais inválidas, tente novamente!')
+    })
+  })
+
+
+  context('sem informar código do instagram', () => {
+    it('deve informar que código do instagram é obrigatório', () => {
+      const user = {
+        password: "pwd123"
+      }
+
+      cy.visit('/')
+
+
+      cy.get('input[name=password]').type(user.password)
+
+      cy.contains('button', 'Entrar').click()
+
+      cy.modalHaveText('Por favor, informe o seu código do Instagram!')
+    })
+  })
+
+  context('sem informar senha', () => {
+    it('deve informar que a senha é obrigatória', () => {
+      const user = {
+        instagram: "@raphilskerj"
+      }
+
+      cy.visit('/')
+
+      cy.get('input[name=instagram]').type(user.instagram)
+
+      cy.contains('button', 'Entrar').click()
+
+      cy.modalHaveText('Por favor, informe a sua senha secreta!')
+    })
+  })
+
+  context('sem informar nenhum dos campos', () => {
+    it('deve solicitar para informar as credenciais', () => {
+
+      cy.visit('/')
+      
+      cy.contains('button', 'Entrar').click()
+
+      cy.modalHaveText('Por favor, informe suas credenciais!')
+    })
   })
 })
